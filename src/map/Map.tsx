a9bb6e2c931593c7d6a324c6e08444cc42355d71
CatSkald <CatSkald@users.noreply.github.com>
2024-10-03 23:49:45 +0200

    Enable coordinates in URL.

--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,11 +1,17 @@
-import { TileLayer, MapContainer, ZoomControl } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-hash";
+import { TileLayer, MapContainer, ZoomControl } from "react-leaflet";
 import { latLngBounds } from "leaflet";
+import * as L from "leaflet";
 import BuildingsOverlay from "./BuildingsOverlay.tsx";
 
 export default function Map() {
   const kolomyiaBounds = latLngBounds([48.6184, 24.9379], [48.4868, 25.1415]);
   const initialZoom = 16;
+  const enableCoordinatesInUrl = (map: L.Map) => {
+    L.hash(map);
+  };
+
   return (
     <MapContainer
       center={[48.525, 25.0373]}
@@ -14,6 +20,7 @@ export default function Map() {
       zoomControl={false}
       maxBoundsViscosity={0.7}
       maxBounds={kolomyiaBounds}
+      ref={enableCoordinatesInUrl}
     >
       <TileLayer url="http://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}" />
       <ZoomControl position="topright" />
---
+++ src/map/leaflet-hash.d.ts
@@ -0,0 +1,3 @@
+declare namespace L {
+  export function hash(map: Map): unknown;
+}

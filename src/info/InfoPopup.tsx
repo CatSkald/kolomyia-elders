3e9f75d7555d3dc5573b03f411152ee6937dbf9d
CatSkald <CatSkald@users.noreply.github.com>
2025-04-23 23:12:36 +0200

    Add close button to popup.

--- src/info/InfoPopup.tsx
+++ src/info/InfoPopup.tsx
@@ -1,10 +1,11 @@
 import "./InfoPopup.css";
+
 import { InfoCircleFill, XCircleFill } from "react-bootstrap-icons";
 import Popup from "reactjs-popup";
 import { mapSources } from "../utils";
 import { sources } from "../data/sources";
 import { buttonSize } from "../themes";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 
 const InfoPopup = () => {
   const [open, setOpen] = useState(false);
@@ -26,29 +27,42 @@ const InfoPopup = () => {
         overlayStyle={{ zIndex: 1000 }}
         modal
       >
-        <span>
-          Наш сайт створено з метою популяризації історії міста Коломиї.
-          Інформація постійно доповнюється і допрацьовується.
-        </span>
-        <hr />
-        <span style={{ fontWeight: "bold" }}>Використані джерела:</span>
-        <ol style={{ marginTop: "0.5rem", paddingLeft: "1.5rem" }}>
-          {mapSources(sources).map((item, index) => (
-            <li key={index}>
-              <span>{item.title}</span>
-            </li>
-          ))}
-        </ol>
-        <hr />
-        <span>Ідея сайту навіяна проектами</span> “
-        <a href="https://map.klym.uk/" rel="external" target="_blank">
-          Вік&nbsp;будинків&nbsp;Станиславова
-        </a>
-        ” та “
-        <a href="https://kolobramy.com.ua/" rel="external" target="_blank">
-          коло&nbsp;брами
-        </a>
-        ”.
+        {
+          ((close: () => void) => (
+            <>
+              <div className="button close" onClick={close}>
+                <XCircleFill size={buttonSize} />
+              </div>
+              <span>
+                Наш сайт створено з метою популяризації історії міста Коломиї.
+                Інформація постійно доповнюється і допрацьовується.
+              </span>
+              <hr />
+              <span style={{ fontWeight: "bold" }}>Використані джерела:</span>
+              <ol style={{ marginTop: "0.5rem", paddingLeft: "1.5rem" }}>
+                {mapSources(sources).map((item, index) => (
+                  <li key={index}>
+                    <span>{item.title}</span>
+                  </li>
+                ))}
+              </ol>
+              <hr />
+              <span>Ідея сайту навіяна проектами</span> “
+              <a href="https://map.klym.uk/" rel="external" target="_blank">
+                Вік&nbsp;будинків&nbsp;Станиславова
+              </a>
+              ” та “
+              <a
+                href="https://kolobramy.com.ua/"
+                rel="external"
+                target="_blank"
+              >
+                коло&nbsp;брами
+              </a>
+              ”.
+            </>
+          )) as unknown as ReactNode
+        }
       </Popup>
     </>
   );

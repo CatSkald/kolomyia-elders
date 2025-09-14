9d2beb5fc682cc95073aa794f5dcdebf6287e38c
CatSkald <CatSkald@users.noreply.github.com>
2025-09-14 09:28:38 +0200

    Update map style URLs.

--- src/map/MaptilerConfig.ts
+++ src/map/MaptilerConfig.ts
@@ -1,7 +1,7 @@
 export const config = {
   darkStyle:
-    "https://api.maptiler.com/maps/47df0403-9aaf-42c7-b147-c51776853d57/style.json",
+    "https://api.maptiler.com/maps/01994715-1d17-713b-ade5-bb3c25d2e329/style.json",
   lightStyle:
-    "https://api.maptiler.com/maps/1a59e8d4-75e0-40d1-951b-ed165c8779db/style.json",
+    "https://api.maptiler.com/maps/01994715-f124-7a6f-af6d-4f1e9a78ac9a/style.json",
   x: "",
 };
